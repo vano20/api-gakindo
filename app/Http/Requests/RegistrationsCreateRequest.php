751b712d492506adea6ac80a237f83dfed5ed783
Savano Miatama <vano_202228@yahoo.com>
2024-03-10 15:31:48 +0700

    feat: validation - add registration request validation custom message

--- app/Http/Requests/RegistrationsCreateRequest.php
+++ app/Http/Requests/RegistrationsCreateRequest.php
@@ -43,4 +43,27 @@ class RegistrationsCreateRequest extends FormRequest
             "errors" => $validator->getMessageBag()
         ], 400));
     }
+
+    public function messages()
+    {
+        return [
+            'company_name.required' => 'Nama perusahaan diperlukan',
+            'company_name.max' => 'Jumlah karakter nama perusahaan tidak boleh melebihi 100 karakter.',
+            'contact_person.required' => 'Nama penanggung jawab diperlukan',
+            'contact_person.max' => 'Jumlah karakter nama penanggung jawab tidak boleh melebihi 100 karakter.',
+            'email.required' => 'Email diperlukan',
+            'email.max' => 'Jumlah karakter email tidak boleh melebihi 200 karakter.',
+            'email.email' => 'Masukkan email yang valid.',
+            'phone_number.required' => 'Nomor handphone diperlukan',
+            'phone_number.max' => 'Jumlah karakter nomor handphone tidak boleh melebihi 20 karakter.',
+            'position.required' => 'Jabatan diperlukan',
+            'position.max' => 'Jumlah karakter jabatan tidak boleh melebihi 100 karakter.',
+            'npwp.required' => 'NPWP diperlukan',
+            'npwp.max' => 'Jumlah karakter NPWP tidak boleh melebihi 16 karakter.',
+            'npwp.unique' => 'NPWP telah terdaftar.',
+            'qualification.required' => 'Kualifikasi diperlukan',
+            'qualification.max' => 'Jumlah karakter kualifikasi tidak boleh melebihi 100 karakter.',
+            'province_id.required' => 'Provinsi dan Kabupaten/Kota diperlukan',
+        ];
+    }
 }
